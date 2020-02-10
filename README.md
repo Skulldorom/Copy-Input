8b6330a8eaefc53db425a89b4ce822c78a271ec3
Skulldorom <adam.desa@hotmail.com>
2020-02-10 09:06:54 +0000

    First version

---
+++ README.md
@@ -0,0 +1,42 @@
+# COPY 2 INPUT
+
+jquery and css styler for html element to copy text to clipboard from a label or span
+
+## Getting Started
+
+Please install the prerequisites listed below to use, an example is attached in index.html
+
+The "copy-input" and "copy-value" classes are requied for the code to work
+
+The minimum code is as follows:
+```
+<div class="field copy-input">
+  <div class="control">
+    <label class="input copy-value">Hello World</label> 
+  </div>
+</div>
+
+```
+style can be edited using blumas classes
+
+### Prerequisites
+
+FontAwsome
+Jquery
+Bulma CSS
+
+```
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+    <script language="JavaScript" type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
+```
+
+## Built With
+
+* [BULMA](https://bulma.io/) - Main CSS
+* [JQUERY](https://jquery.com/) - Javascript Dependency
+* [FONTAWSOME](https://fontawesome.com/) - Used to generate Icons
+
+## Acknowledgments
+
+* Bulma for the amazing css code
---
+++ copy.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    //create copy input
+    $('.copy-input').each(function(){
+        create_copy($(this).find('.control'));
+    })
+
+    function create_copy(control){
+      control.addClass('has-icons-right')
+      control.html(control.html()+'<span class="icon is-right copyicon"><i class="fas fa-copy"></i></span>')
+    }
+
+    $('.copy-input').on('click',function(){
+        value = $(this).find('.copy-value').html()
+        input = $(this).find('.copy-value')
+        copy_text(value);
+        icon = $(this).find('.copyicon')
+        icon.finish()
+        icon.animate({width:'toggle'},250,function(){
+            icon.html('<span class="tag" style="margin-right:10px;">Copied!</span><i class="fas fa-check"></i>')
+            input.addClass("copyclick")
+            icon.addClass('clicked')
+            icon.animate({width:'toggle'},500,function(){
+                icon.delay(1000).animate({width:'toggle'},500,function(){
+                    input.removeClass("copyclick")
+                    icon.removeClass('clicked')
+                    icon.html('<i class="fas fa-copy"></i>')
+                    icon.animate({width:'toggle'},250)
+                })
+            })
+        })
+    });
+});
+
+function copy_text(text) {
+    var textArea = document.createElement("textarea");
+    var MainElement = $('body');
+    textArea.value = text;
+    textArea.setAttribute('readonly', '');
+    textArea.style.position = 'absolute';
+    textArea.style.left = '-9999px';
+    MainElement.append(textArea);
+    textArea.focus();
+    textArea.select();
+    try {
+        document.execCommand('copy');
+    }
+    catch (err) {
+        alert("Failed to copy text " + err);
+    }
+    MainElement.remove(textArea)    
+}
